d2cb3168d807ef03729d1100e6c2f1b058c93944
ChowdhuryFatema <fatemachowdhury318@gmail.com>
2024-04-27 18:30:32 +0600

    Some Design Added

--- src/components/TouristsSpotCard/TouristsSpotCard.jsx
+++ src/components/TouristsSpotCard/TouristsSpotCard.jsx
@@ -1,32 +1,36 @@
-import { FaRegClock } from "react-icons/fa";
+import { IoLocationOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
 
 const TouristsSpotCard = ({touristsSpot}) => {
-    const {_id} = touristsSpot;
+    const {
+        _id,
+        image,
+        description, 
+        country_Name,
+        tourists_spot_name
+    } = touristsSpot;
     return (
         <div className="card bg-base-100 shadow-xl">
             <div className="relative rounded-2xl">
                 <figure>
-                    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+                    <img src={image} alt="Shoes" />
                 </figure>
                 <p className="card-lavel flex items-center gap-2 bg-red absolute py-3 px-7 -bottom-6 left-6 text-white">
-                    <FaRegClock />
-                    <span>2 Day & 1 night</span>
+                <IoLocationOutline size={20}/>
+                    <span>{country_Name}</span>
                 </p>
             </div>
 
             <div className="card-body">
-                <h2 className="card-title">
-                    Shoes!
+                <h2 className="font-semibold text-3xl">
+                    {tourists_spot_name}
                 </h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Fashion</div>
-                    <div className="badge badge-outline">Products</div>
-                </div>
-                <div>
-                    <Link to={`/touristsSpots/${_id}`} className="btn border-red-500 bg-transparent rounded-none btn-overlay btn-overlay hover:bg-transparent relative text-red hover:text-white duration-500">
+                
+                <p className="my-2">{description}</p>
+                
+                <div className="pt-1">
+                    <Link to={`/touristsSpots/${_id}`} className="btn border-red-500 bg-transparent rounded-sm btn-overlay btn-overlay hover:border-red-500 hover:bg-transparent relative text-red hover:text-white duration-500">
                         <span className="z-30 text-lg font-semibold">
                             View Details
                         </span>
--- src/pages/AllTouristsSpot/AllTouristsSpot.jsx
+++ src/pages/AllTouristsSpot/AllTouristsSpot.jsx
@@ -5,17 +5,19 @@ import TouristsSpotCard from "../../components/TouristsSpotCard/TouristsSpotCard
 
 const AllTouristsSpot = () => {
 
-    const {data} = UseTourSpots();
+    const { data } = UseTourSpots();
 
     return (
         <div className="max-w-7xl mx-auto px-5">
-            <h2>AllTouristsSpot{data.length}</h2>
+            <div className="my-10">
+                <h2 className="text-5xl font-semibold text-center">All Tourists Spots</h2>
+            </div>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 pb-10">
                 {
                     data.map(touristsSpot => <TouristsSpotCard
-                    key={touristsSpot._id}
-                    touristsSpot={touristsSpot}></TouristsSpotCard>)
+                        key={touristsSpot._id}
+                        touristsSpot={touristsSpot}></TouristsSpotCard>)
                 }
             </div>
         </div>
